877d854a8c3bc86726f62a1183e68c30f85e6b41
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-05-22 08:07:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# `@sebastienrousseau/mocha-config`
+
+> TODO: description
+
+## Usage
+
+```
+const mochaConfig = require('@sebastienrousseau/mocha-config');
+
+// TODO: DEMONSTRATE API
+```
---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "diff": true,
+  "exit": true,
+  "extension": ["js"],
+  "package": "./package.json",
+  "spec": ["./test/*.js", "./test/**/*.js"],
+  "slow": "75",
+  "timeout": "60000"
+};
