9f1b83b468b3b6249f449f76896cf9a50a6c8ad7
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-06-05 11:05:21 +0100

    ci: v.0.0.2

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = {
   "diff": true,
   "exit": true,
-  "extension": ["js"],
+  "extension": ["js", "ts"],
   "package": "./package.json",
-  "spec": ["./test/*.js", "./test/**/*.js"],
+  "spec": ["./test/*.js", "./test/**/*.js", "./test/*.ts", "./test/**/*.ts"],
   "slow": "75",
   "timeout": "60000"
 };
